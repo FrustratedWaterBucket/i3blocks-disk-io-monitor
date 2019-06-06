b155d72252786e49e8d44e27c2d5a098c025b01d
José Soares <josepdsoares@gmail.com>
2019-06-06 01:09:50 +0100

    Read from diskstats, filtering by device ? - Aggregating all columns results in duplicated counts? (eg: writing to sda1 also increases sda)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # i3blocks-disk-io-monitor
 i3 Blocks disk IO real time monitor
+
+
+
+# Compiling
+
+> cargo build --release
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,73 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::thread;
+use std::time;
+use std::time::SystemTime;
+use std::env;
+
+struct DiskIOStats {
+    written_sectors: u64,
+    read_sectors: u64,
+    time: SystemTime
+}
+
+// 829
+// 837
 
 fn main() {
-    let lines = lines_from_file("/proc/diskstats").expect("oitat");
-    let target_disk = "sdb";
 
-    let filtered_lines =  lines
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Must have device name as first argument")
+    }
+
+    let device_name = args[1].clone();
+
+    let mut last_stat = get_io_stats(&device_name);
+
+    loop {
+        let current_read = get_io_stats(&device_name);
+        let time_difference_in_seconds = last_stat.time.elapsed().expect("error measuring elapsed time").as_secs();
+
+        if time_difference_in_seconds > 0 {
+            let w_sectors = current_read.written_sectors - last_stat.written_sectors;
+            let r_sectors = current_read.read_sectors - last_stat.read_sectors;
+
+            println!("w: {} kB/s r: {} kB/s",
+                (w_sectors*512/1024)/time_difference_in_seconds, 
+                (r_sectors*512/1024)/time_difference_in_seconds);
+        }
+
+        last_stat = current_read;
+        thread::sleep(time::Duration::from_secs(1));
+    }
+}
+
+fn get_io_stats(target_device: &String) -> DiskIOStats {
+
+    let lines = 
+        lines_from_file("/proc/diskstats")
+        .expect("unable to read /proc/diskstats")
         .into_iter()
-        .filter(|x| x.contains(target_disk))
-        .collect::<Vec<String>>();
+        .filter(|x| x.split_whitespace().collect::<Vec<&str>>()[2] == target_device);
+    
+    // 5r,9w
+    let mut all_read_sectors = 0;
+    let mut all_written_sectors = 0;
 
-    let target_line;
+    for line in lines {
+        let line_split = line.split_whitespace().collect::<Vec<&str>>();
 
-    if filtered_lines.len() >= 1 {
-        target_line = filtered_lines.first().expect("should exist");
+        all_read_sectors += line_split[5].parse::<u64>().expect("unexpected format on /proc/diskstats");
+        all_written_sectors += line_split[9].parse::<u64>().expect("unexpected format on /proc/diskstats");
+    }    
+
+    return DiskIOStats {
+        written_sectors: all_written_sectors,
+        read_sectors: all_read_sectors,
+        time:  SystemTime::now()
     }
-    else {
-        panic!("Non existant disk");
-    }
-
-    println!("{}", target_line);
 }
 
 fn lines_from_file<P>(filename: P) -> Result<Vec<String>, io::Error>
